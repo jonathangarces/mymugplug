09ca370004077982b15b4e8e4df225b79ebe2012
Jonathan <jonathang@Jonathans-MBP.attlocal.net>
2020-01-03 23:14:05 -0500

    test desc

--- listener.js
+++ listener.js
@@ -42,7 +42,7 @@ async function addData(tweet) {
         type: "simple",
         regular_price: "15.99",
         description: tweet.user.url,
-        short_description: tweet.user.screen_name ,
+        short_description: tweet.data.text,
         categories: [
             {
                 id: 21

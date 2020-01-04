30e27c615efd1ff9fb6b67f350e73866165ed8be
Jonathan <jonathang@Jonathans-MBP.attlocal.net>
2020-01-03 21:45:02 -0500

    changed description

--- listener.js
+++ listener.js
@@ -41,7 +41,8 @@ async function addData(tweet) {
         name: "Custom Mug Design For " + tweet.user.screen_name,
         type: "simple",
         regular_price: "15.99",
-        description: tweet.user.url,
+        //description: tweet.user.url
+        description: tweet.user.text,
         short_description: "",
         categories: [
             {

b1512d005b83dcaa41a7c29f7a865f817b3b2328
Jonathan <jonathang@Jonathans-MBP.attlocal.net>
2020-01-03 22:27:48 -0500

    testing

--- listener.js
+++ listener.js
@@ -41,9 +41,8 @@ async function addData(tweet) {
         name: "Custom Mug Design For " + tweet.user.screen_name,
         type: "simple",
         regular_price: "15.99",
-        //description: tweet.user.url
-        description: tweet.user.text,
-        short_description: "",
+        description: tweet.user.url,
+        short_description: "hello",
         categories: [
             {
                 id: 21
